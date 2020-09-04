63726575f0381d14998194dff8a7b4dffe649661
namantia1@gmail.com <nickamantia@gmail.com>
2020-09-04 12:13:36 -0400

    first commit

---
+++ src/Components/Form/Form.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react'
+
+export default class Form extends Component{
+    constructor(){
+        super()
+        this.state = {
+            
+        }
+    }
+
+
+
+
+    render(){
+
+        return(
+            <div>
+                <Form/>ß
+            </div>
+        )
+    }
+}
---
+++ src/Components/Product/Product.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react'
+
+export default class Product extends Component{
+    constructor(){
+        super()
+        this.state = {
+            
+        }
+    }
+
+
+    render(){
+
+        return(
+            <div>
+                <Product />
+            </div>
+        )
+    }
+}
