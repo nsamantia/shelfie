3d8453ffef2044a1c2424e4b82b195bf78d56a00
namantia1@gmail.com <nickamantia@gmail.com>
2020-09-04 13:52:19 -0400

    bd

--- server/index.js
+++ server/index.js
@@ -1,12 +1,19 @@
-
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const SERVER_PORT = 4000
+const massive = require('massive')
 
-// app.get('/', (res, req) => {
-//     res.send('hi')
-// })
+const {CONNECTION_STRING} = process.env
+
+app.use(express.json())
 
 app.listen(SERVER_PORT, () => {
     console.log(`What up from ${SERVER_PORT}`)
 })
+
+massive({connectionString: CONNECTION_STRING, ssl:{rejectUnauthorized: false}}).then(dbInstance => {
+    app.set('db', dbInstance)
+}).catch(err => console.log(err))
+
+
